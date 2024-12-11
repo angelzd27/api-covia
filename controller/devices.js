d724fb1b5cc07f8440029804c80f5315ec2e6cf9
decidesuici <eduardx62@outlook.com>
2024-12-11 14:50:45 -0600

    Aquí funcionan los devices c:

--- controller/devices.js
+++ controller/devices.js
@@ -16,6 +16,7 @@ export const getDevices = async (request, response) => {
             "terid": devices
         }
     });
+
     try {
         const token = request.headers['authorization'];
 
@@ -87,6 +88,106 @@ export const getDevices = async (request, response) => {
         }
     }
 };
+
+export const allDevices = async (request, response) => {
+    const { authorization } = request.headers
+
+    if (!authorization)
+        return response.status(401).json({ error: true, data: 'auth_token_not_provider' })
+
+    let decoded
+    let devicesDatabase = []
+    let devicesAPI = []
+
+    try {
+        decoded = jwt.verify(authorization, process.env.SECRET_KEY)
+    } catch (err) {
+        return response.status(400).json({ error: true, data: 'jwt_malformed' })
+    }
+
+    const { id, key } = decoded
+
+    const queryGetGroups = `
+        SELECT groups.id, groups.group
+        FROM groups
+        JOIN user_group ON groups.id = user_group.group_id
+        WHERE groups.status = true AND user_group.user_id = $1
+    `
+
+    const resultGroups = (await pool_db.query(queryGetGroups, [id])).rows
+
+    for (const group of resultGroups) {
+        const queryGetDevices = `
+            SELECT *
+            FROM devices
+            JOIN group_device ON devices.id = group_device.device_id
+            WHERE devices.status = true AND group_device.group_id = $1
+        `
+
+        const resultDevices = (await pool_db.query(queryGetDevices, [group.id])).rows
+
+        devicesDatabase = devicesDatabase.concat(resultDevices)
+    }
+
+    const configResponseAPI = {
+        method: 'POST',
+        url: 'http://74.208.169.184:12056/api/v1/basic/gps/last',
+        headers: {
+            'accept': 'application/json',
+        },
+        data: {
+            key: key,
+            terid: devicesDatabase.map(device => device.id)
+        }
+    }
+
+    const responseAPI = (await axios(configResponseAPI)).data
+
+    devicesAPI = responseAPI.data
+
+    const combinedDevices = devicesDatabase.map(device => {
+        const apiData = devicesAPI.find(api => api.terid === device.id);
+
+        const cleanedGpsData = apiData
+            ? {
+                ...apiData,
+                terid: undefined,
+                gpslat: undefined,
+                gpslng: undefined,
+                altitude: undefined,
+                speed: undefined,
+                direction: undefined
+            }
+            : {}
+
+        return {
+            id: device.id,
+            name: device.name,
+            uniqueId: device.phone,
+            status: device.device_status,
+            latitude: apiData?.gpslat || null,
+            longitude: apiData?.gpslng || null,
+            altitude: apiData?.altitude || null,
+            speed: apiData?.speed || 0,
+            previousAngle: apiData?.direction || 0,
+            fuel_id: device.fuel_id,
+            km_per_liter: device.km_per_liter,
+            is_dvr: device.is_dvr,
+            channelcount: device.channelcount,
+            status: device.status,
+            last_update: device.last_update,
+            imei: device.imei,
+            created_at: device.created_at,
+            attributes: {
+                cleanedGpsData
+            }
+        }
+    })
+
+    console.log(combinedDevices)
+
+    return response.status(200).json({ error: false, data: combinedDevices });
+}
 
 export const geCamerasUrl = async (request, response) => {
     const { authorization } = request.headers
--- routes/devices.js
+++ routes/devices.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { geCamerasUrl, getDevices } from '../controller/devices.js'
+import { geCamerasUrl, getDevices, allDevices } from '../controller/devices.js'
 
 export const router_devices = Router()
 
+router_devices.get('/devices', allDevices)
 router_devices.get('/allDevices', getDevices)
 router_devices.post('/getUrls', geCamerasUrl)
