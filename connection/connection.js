fde223624d76fc6a345950cb7ca947d63f101ab7
angelzd27 <angeltj27@gmail.com>
2025-01-03 13:13:48 -0600

    bd pwd encrypted

--- connection/connection.js
+++ connection/connection.js
@@ -1,5 +1,6 @@
 import pg from 'pg'
 import dotenv from 'dotenv'
+import { decrypt } from '../utils/encrypt.js'
 
 dotenv.config()
 
@@ -7,6 +8,6 @@ export const pool_db = new pg.Pool({
     user: process.env.USER_PG,
     host: process.env.HOST_PG,
     database: process.env.DATABASE_PG,
-    password: process.env.PASSWORD_PG,
+    password: decrypt(process.env.PASSWORD_PG),
     port: process.env.PORT_PG
 })
